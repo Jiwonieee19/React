c3a7f29654fb115edceff3b5055dbe4ecb25c8bc
Jiwonieee19 <ryanjaycompuesto@gmail.com>
2026-01-22 22:45:57 +0800

    day 8

---
+++ useEffectHook/src/App.jsx
@@ -0,0 +1,32 @@
+//useEffect() = React Hook that tells React DO SOME CODE WHEN (pick one):
+//              This component re-renders
+//              This component mounts
+//              The state of a value
+
+// useEffect(function, [dependencies])
+
+//1. useEffect(() => {})               //Runs after every re-render
+//2. useEffect(() => {}, [])           //Runs only on mount
+//3. useEffect(() => {}, [value])      //Runs on mount + when value changes
+
+//USES
+//1. Event Listeners
+//2. DOM Manipulation
+//3. Subscriptions (real-time updates)
+//4. Fetching Data from an API
+//5. Clean up when a component unmounts
+
+// mounting is adding a component in a DOM, can also remove a component
+
+import MyComponent from './MyComponent.jsx'
+
+function App() {
+
+  return (
+    <>
+      <MyComponent />
+    </>
+  )
+}
+
+export default App
---
+++ useEffectHook/src/MyComponent.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react'
+
+function MyComponent() {
+    const [count, setCount] = useState(0);
+    // RUNS EVERY RE-RENDER
+    // useEffect(any function, [dependencies]);
+    // useEffect(() => {
+    //     document.title = `Count sa useEffect: ${count}`
+    // }) // runs every re-render, EVERY component thats been rendered, this will act too
+
+    //RUNS ONLY IF MOUNT
+    // useEffect(() => {
+    //     document.title = `One Run Only ${count}`
+    //     //count wont show here since it only run after first putting something in the count, or first onClick
+    //     //One-And-Done Code
+    // }, []) //if absent ning empty array, it will run every rerender
+
+    //RUNS ONLY ON MOUNT + WHEN VALUE IS CHANGE
+    // useEffect(() => {
+    //     document.title = `Count with Value: ${count}`
+    // }, [count]) //runs on mount only + when specific value is change
+    // //if count is changed, it will run
+
+
+    const [color, setColor] = useState("green");
+
+    //MULTI DEPENDENCIES
+    // useEffect(() => {
+    //     document.title = `Multi-Dependencies Count: ${count} ${color}`
+    // }, [count]) //if count palang naa, ddto pa mo change ang color na naa sa title if makabalik ug click kang dount, during click ka color, dli ma change ang title
+
+    useEffect(() => {
+        document.title = `Count and Color: ${count} ${color}`
+    }, [count, color]) //if these are the dependencies, mo render siya every time naay ma change anang duha
+
+    return (<>
+        <p style={{ color: color }}>Count: {count}</p>
+        <button onClick={() => setCount(count + 1)}>Add</button>
+        <button onClick={() => setCount(count - 1)}>Subtract</button>
+        <br />
+        <button onClick={() => setColor(color === "green" ? "red" : "green")}>Change Color</button>
+    </>)
+}
+
+// WHY USE useEffectHook??
+// to look more organize and readable if when this function will run and so on, 
+// since removing the use effect on the top, it will still work (docu.title = `dkfdkdkf`) but will always run every re-render on the code
+// to manipulate better of the datas and codes
+
+export default MyComponent
