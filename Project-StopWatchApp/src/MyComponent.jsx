1ba49328653fbcac87036f3cc6d2eb94d8a26861
Jiwonieee19 <ryanjaycompuesto@gmail.com>
2026-01-25 01:24:33 +0800

    day 10

--- Project-StopWatchApp/src/MyComponent.jsx
+++ Project-StopWatchApp/src/MyComponent.jsx
@@ -7,13 +7,45 @@ function MyComponent() {
     const intervalIdRef = useRef(null);
     const startTimeRef = useRef(0);
 
+    useEffect(() => {
+
+        if (isRunning) {
+            intervalIdRef.current = setInterval(() => {
+                setElapsedTime(Date.now() - startTimeRef.current)
+            }, 10)
+            //then pagka balik dri since nag true na, date now - start = seconds, every 10ms HAHAHA gets
+        }
+
+        return () => {
+            clearInterval(intervalIdRef.current);
+        }
+
+    }, [isRunning]);
+
+    function start() {
+        setIsRunning(true);
+        startTimeRef.current = Date.now() - elapsedTime; //stores current TIME in seconds (17,000,000,000)
+    }
+
+    function stop() {
+        setIsRunning(false);
+    }
+
+    function reset() {
+        setElapsedTime(0);
+        setIsRunning(false);
+    }
+
     function formatTime() {
-        const minutes = 0;
-        const seconds = 0;
-        const milliseconds = 0;
+        let hours = Math.floor(elapsedTime / (1000 * 60 * 60))
+        let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+        let seconds = Math.floor(elapsedTime / (1000) % 60);
+        let milliseconds = Math.floor((elapsedTime % 1000) / 10);
+
+        // hours = String(hours).padStart(2, "0"); //mao ni gigamit niya this time, not the padZero written function
 
         return (
-            `${padZero(minutes)} : ${padZero(seconds)} : ${padZero(milliseconds)}s`
+            `${padZero(minutes)}:${padZero(seconds)}:${padZero(milliseconds)}s`
         )
     }
 
@@ -21,15 +53,13 @@ function MyComponent() {
         return nums < 10 ? `0${nums}` : nums;
     }
 
-    function startButton() {
-
-    }
-
     return (<div className='main-container'>
-        <span>{formatTime()}</span>
-        <br></br>
-        <button onClick={startButton()}>START</button>
-        <button onClick={startButton()}>{startTime.getHours()}</button>
+        <div><span>{formatTime()}</span></div>
+        <div className='button-container'>
+            <button onClick={start} className='start-button'>START</button>
+            <button onClick={stop} className='stop-button'>STOP</button>
+            <button onClick={reset} className='reset-button'>RESET</button>
+        </div>
     </div>)
 }
 
